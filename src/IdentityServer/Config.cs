18c34c908fab9c6004aa83bf76942294e13d391f
Jacob Weaver <jacobsweaver@gmail.com>
2023-02-13 13:09:55 -0600

    Switched to identity server duende; Setup minimal clients and apis; Now runs;

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { 
+                new ApiScope("Catalog.WebApi", "Catalog.WebApi"),
+                new ApiScope("Catalog.WebApp", "Catalog.WebApp"),
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            {
+                new Client
+                {
+                    ClientId = "Catalog.WebApp",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        // TODO: Change to real secret
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes =
+                    { 
+                        "Catalog.WebApi"
+                    }
+                }
+            };
+}
